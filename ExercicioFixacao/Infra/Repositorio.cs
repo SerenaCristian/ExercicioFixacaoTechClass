63f927809c8b3a264af5dab878810717ea5d7343
SerenaCristian <devcristian02@gmail.com>
2024-10-08 22:06:03 -0300

    subindo alterações

--- ExercicioFixacao/Infra/Repositorio.cs
+++ ExercicioFixacao/Infra/Repositorio.cs
@@ -1,6 +1,16 @@
-﻿namespace ExercicioFixacao.Infra
+﻿using ExercicioFixacao.Core.Entidades;
+using System.Collections.Generic;
+
+namespace ExercicioFixacao.Infra
 {
     internal class Repositorio
     {
+        private static List<Cliente> clientes = new List<Cliente>();
+        public void Add(Cliente cliente)
+        {
+            clientes.Add(cliente);
+        }
+
+
     }
 }
