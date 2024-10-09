f8a3183c35bb99e4638acbe03eb549906668a2df
SerenaCristian <devcristian02@gmail.com>
2024-10-08 22:23:25 -0300

    Service

--- ExercicioFixacao/Servico/servico.cs
+++ ExercicioFixacao/Servico/servico.cs
@@ -1,6 +1,19 @@
-﻿namespace ExercicioFixacao.Servico
+﻿using ExercicioFixacao.Core.Entidades;
+using ExercicioFixacao.Infra;
+using System;
+
+namespace ExercicioFixacao.Servico
 {
     internal class servico
     {
+        private Repositorio _Repositorio;
+
+        public void Add(Cliente cliente)
+        {
+            _Repositorio = new Repositorio();
+            _Repositorio.Add(cliente);
+           
+        }
+
     }
 }
