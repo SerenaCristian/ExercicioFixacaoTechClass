e383a48f7a0a4bcb3311d8fa6544484856b23158
SerenaCristian <devcristian02@gmail.com>
2024-10-08 21:36:50 -0300

    Montando as camadas da aplicação

---
+++ ExercicioFixacao/Core/Entidades/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao.Core.Entidades
+{
+    internal class Cliente
+    {
+    }
+}
---
+++ ExercicioFixacao/Infra/Repositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao.Infra
+{
+    internal class Repositorio
+    {
+    }
+}
